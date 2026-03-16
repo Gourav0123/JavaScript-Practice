6a47be421716f2029ec99fe5bf398bcc5c67e174
Gourav <gourav@conjoinix.com>
2026-03-16 11:49:52 +0530

    Added Copy paste from another folder JS Files from 01 to 04

---
+++ JavaScript Codes/04_comparision.js
@@ -0,0 +1,25 @@
+console.log("2" > 1);
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// ===
+console.log("2" === 2);  //false - it will check datatype as well
+
+// 7 types : String, Number, Boolean
+
+const heros = ["ShakiMan","Nagraj","Doga"];
+let myObj = {
+    name : "Gourav",
+    age : 23,
+}
+
+const myFunction = function(){
+    console.log("Hello World");
+}
+
+console.log(typeof anotherId);
+console.log("Hello added in");
