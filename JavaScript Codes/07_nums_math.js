1a63609206dde266966224c9373d7e0814ed4360
Gourav <gourav@conjoinix.com>
2026-03-18 17:55:32 +0530

    code modified 07

--- JavaScript Codes/07_nums_math.js
+++ JavaScript Codes/07_nums_math.js
@@ -1,5 +1,14 @@
-const score = 400;
+const score = 400000;
 
-const balance = new Number(100)
-console.log(score)
+const balance = new Number(100000)
+console.log(score);
 console.log(balance);
+
+console.log(balance.toString().length);
+console.log(balance.toFixed(2));
+
+const otherNumber = 123.8966;
+console.log(otherNumber.toPrecision(4));
+
+const hundreds = 100000000
+console.log(hundreds.toLocaleString('en-IN'));
